e4d41548501aad378f627e9787dbb54595baafec
“Arunesh <as8175802986@gmail.com>
2021-11-09 23:13:42 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparison");
+            Console.ReadLine();
+        }
+    }
+}
